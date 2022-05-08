39ed5c5df3c63ff3bdcc947ce6d1960fa5a08ad8
hhhanying <48273181+hhhanying@users.noreply.github.com>
2022-05-08 13:25:23 -0500

    add BPM

--- R/Normal.R
+++ R/Normal.R
@@ -310,6 +310,132 @@ SPM_membership_Normal <- function(X, Y, Lambda, Mu, a, rho, Ts, ntrace, nchain, 
   G
 }
 
+BPM_training_Normal <- function(X, b, alpha, mu_Mu, sigma2_Mu, alpha_Lambda, beta_Lambda, ntopic, ntrace, nchain, nskip){
+  BPM_Normal_stancode <-"
+  data {
+    int<lower=0> N;  // size of training set
+    int<lower=0> ntopic;
+    int<lower=0> d; //dim of data
+    
+    matrix[N, d] X;
+    
+    real<lower=0> b;
+    vector<lower=0>[ntopic] alpha;
+    matrix[ntopic, d] mu_Mu;
+    matrix[ntopic, d] sigma2_Mu;
+    matrix[ntopic, d] alpha_Lambda;
+    matrix[ntopic, d] beta_Lambda;
+  }
+  parameters {
+    real<lower=0> a;
+    simplex[ntopic] rho;
+    
+    simplex[ntopic] U[N];
+    
+    matrix[ntopic, d] Mu;
+    matrix<lower=0>[ntopic, d] Lambda;
+  }
+  model{
+    matrix[ntopic, d] Tau;
+    
+    a ~ exponential(b);
+    rho ~ dirichlet(alpha);
+    
+    // topics
+    for (i in 1:ntopic){
+      for(j in 1:d){
+        Lambda[i, j] ~ gamma(alpha_Lambda[i, j], beta_Lambda[i, j]);
+        Mu[i, j] ~ normal(mu_Mu[i, j], sqrt(sigma2_Mu[i, j] / Lambda[i, j]));
+        Tau[i, j] = Mu[i, j] * Lambda[i, j];
+      }
+    }
+    
+    for (i in 1:N){
+      row_vector[d] lambda_X;
+      row_vector[d] tau_X;
+      
+      U[i] ~ dirichlet(a * rho);
+      
+      lambda_X = U[i]' * Lambda;
+      tau_X = U[i]' * Tau;
+      
+      for (j in 1:d){
+        X[i, j] ~ normal(tau_X[j] / lambda_X[j], sqrt(1 / lambda_X[j]));
+      }
+    }
+  }
+  "
+  
+  # calculate needed parameters (avoid too many inputs)
+  N <- length(Y)
+  d <- dim(X)[2]
+  
+  # we assume all hyperparameters for topics are matrices, if they are not, we will duplicate it to make a matrix
+  mu_Mu = trans_to_matrix(mu_Mu, ntopic, d)
+  sigma2_Mu = trans_to_matrix(sigma2_Mu, ntopic, d)
+  alpha_Lambda = trans_to_matrix(alpha_Lambda, ntopic, d)
+  beta_Lambda = trans_to_matrix(beta_Lambda, ntopic, d)
+  
+  # the data to fit
+  dat_fit <- list(
+    N = N,
+    ntopic = ntopic,
+    d = d,
+    
+    X = X,
+    
+    b = b, 
+    alpha = alpha,
+    mu_Mu = mu_Mu,
+    sigma2_Mu = sigma2_Mu,
+    alpha_Lambda = alpha_Lambda,
+    beta_Lambda = beta_Lambda
+  )
+  
+  # sampling
+  fit_train <-rstan::stan(model_code = BPM_Normal_stancode,
+                          data = dat_fit,
+                          chains = nchain,
+                          iter = ntrace)
+  trace <- as.matrix(fit_train)
+  
+  # save results
+  nsample <- dim(trace)[1]
+  nsave <- nsample %/% nskip
+  index_save <- (1:nsave) * nskip
+  trace <- trace[index_save,]
+  
+  res <- list()
+  
+  paras <- c("U", "Lambda", "Mu")
+  d1s <- c(N, K, K)
+  d2s <- c(ntopic, d, d)
+  for (i in 1:3){
+    para <- paras[i]
+    d1 <- d1s[i]
+    d2 <- d2s[i]
+    tem <- matrix(NA, nrow = d1, ncol = d2)
+    for (i1 in 1:d1){
+      for(i2 in 1:d2){
+        varname <- sprintf("%s[%s,%s]", para, i1, i2)
+        tem[i1, i2] <- mean(trace[, varname])
+      }
+    } 
+    res[[para]] <- tem
+  }
+  
+  res[["a"]] <- mean(trace[, "a"])
+  
+  tem <- rep(0, ntopic)
+  for (i in 1:ntopic){
+    varname <- sprintf("rho[%s]", i)
+    tem[i] <- mean(trace[, varname])
+  }
+  res[["rho"]] <- tem
+  
+  res
+}
+
 get_parameters_Normal <- function(X, Y, G, Ts, Lambda, Mu){
   Tau <- Mu * Lambda
   U <- matrix(NA, nrow = N, ncol = K)
